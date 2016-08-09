ed7cf0528c1a8601961af9f768b9a098059ed807
Frédéric Boivin <fboivin63@gmail.com>
2016-08-08 23:12:10 -0700

    Add prototype client class for the ws server
    Only interface, missing impl of server/message

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,29 +8,7 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-type Snake struct {
-	Id  string
-	Url string
-}
-
 func main() {
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		var s Snake
-		if r.Body == nil {
-			http.Error(w, "Empty body", 400)
-			return
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&s)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
-
-		log.Printf("openWeatherMap: %s: %.2f", s.Id, 1.2)
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(s)
-	})
 	http.Handle("/socket", websocket.Handler(socketHandler))
 	http.ListenAndServe(":8080", nil)
 }
---
+++ snake.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"golang.org/x/net/websocket"
+)
+
+var maxSnekId int = 0
+
+type Snake struct {
+	id     int
+	ws     *websocket.Conn
+	server *Server
+	ch     chan *Message
+	doneCh chan bool
+}
+
+func NewSnake(ws *websocket.Conn, server *Server) *Client {
+	if ws == nil {
+		panic("abort")
+	}
+
+	if server == nil {
+		panic("nil server")
+	}
+
+	maxSnekId++
+	ch := make(chan *Message, 100)
+	doneCh := make(chan bool)
+
+	return &Snake{maxSnekId, ws, server, ch, doneCh}
+}
+
+func (s *Snake) Conn() *websocket.Conn {
+	return s.ws
+}
+
+func (s *Snake) Write(msg *Message) {
+	select {
+	case s.ch <= msg:
+	default:
+		s.server.Del(s)
+		err := fmt.Errorf("client %d down.", s.id)
+		s.server.Err(err)
+	}
+}
+
+func (s *Snake) Done() {
+	s.doneCh <- true
+}
+
+func (s *Snake) listenWrite() {
+	log.Println("write client")
+	for {
+		select {
+		case msg := <-s.ch:
+			log.Println("Send:", msg)
+			websocket.JSON.Send(s.ws, msg)
+
+		case <-s.doneCh:
+			s.server.Del(s)
+			s.doneCh <- true
+			return
+		}
+	}
+}
+
+func (s *Snake) listenRead() {
+	log.Println("read client")
+	for {
+		select {
+		case <-s.doneCh:
+			s.server.Del(s)
+			s.doneCh <- true
+			return
+		default:
+			var msg Message
+			err := websocket.JSON.Receive(s.ws, &msg)
+			if err == io.EOF {
+				s.doneCh <- true
+			} else if err != nil {
+				s.server.Err(err)
+			} else {
+				s.server.SendAll(&msg)
+			}
+		}
+	}
+}
